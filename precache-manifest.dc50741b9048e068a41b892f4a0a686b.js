ed07c26a6d91ac5d9e3925ab79cfbc1579f46129
calvin.kurniawan@tokopedia.com <calvin.kurniawan@tokopedia.com>
2019-06-25 10:33:24 +0700

    v0.1.0

---
+++ precache-manifest.dc50741b9048e068a41b892f4a0a686b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdf496ec575b1a2329faebdc775f02e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c32a604155265e1067d5",
+    "url": "/static/css/main.3f11d2e8.chunk.css"
+  },
+  {
+    "revision": "646324ec540f2d2a30ca",
+    "url": "/static/js/2.26d63289.chunk.js"
+  },
+  {
+    "revision": "c32a604155265e1067d5",
+    "url": "/static/js/main.ed1c3d4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
